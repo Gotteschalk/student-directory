eb0f909ba56570f6352af20e061dcb3c1562b1f7
Thomas Ross <thomasross3610@gmail.com>
2019-08-12 16:16:03 +0100

    names printed with index

---
+++ directory_2.rb
@@ -0,0 +1,51 @@
+# Lists the names of students in the Villains Academy in an array
+students = [
+  { name: "Dr. Hannibal Lecter", cohort: :November },
+  { name: "Darth Vader", cohort: :November },
+  { name: "Nurse Ratched", cohort: :November },
+  { name: "Michael Corleone", cohort: :November },
+  { name: "Alex DeLarge", cohort: :November },
+  { name: "The wicked Witch of the West", cohort: :November },
+  { name: "Terminator", cohort: :November },
+  { name: "Freddy Krueger", cohort: :November },
+  { name: "The Joker", cohort: :November },
+  { name: "Joffrey Baratheon", cohort: :November },
+  { name: "Norman Bates", cohort: :November }
+  ]
+
+# Method to print the standard header for students
+def print_header
+  puts "The students of Villains Academy"
+  puts "-----------"
+end
+
+# Method to print all the names in an array of students
+def print(names)
+    names.each.with_index(1) do |student, index|
+      puts "#{index}. #{student[:name]}, #{student[:cohort]} cohort"
+    end
+end
+
+# Method to print the standard footer for students summarising total number of students
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :November}
+    puts "We now have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
